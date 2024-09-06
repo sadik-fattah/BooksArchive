b381438d3fc368ee4b9e45e1ff94dae87db455af
sadik-fattah <sadikfattah5@gmail.com>
2024-09-06 22:31:33 +0100

    ss

--- app/src/main/java/com/guercifzone/booksarchive/SplashScreen.java
+++ app/src/main/java/com/guercifzone/booksarchive/SplashScreen.java
@@ -1,6 +1,12 @@
 package com.guercifzone.booksarchive;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
-
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private TextView textView;
+    private Handler handler = new Handler();
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,33 @@ public class SplashScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        textView = (TextView) findViewById(R.id.textview);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+               while (progressStatus < 100){
+                   progressStatus += 1;
+                   handler.post(new Runnable() {
+                       @Override
+                       public void run() {
+                           progressBar.setProgress(progressStatus);
+                           textView.setText(progressStatus+"/"+progressBar.getMax());
+                           if (progressStatus == 100){
+                               startActivity(new Intent(SplashScreen.this, Home.class));
+                           }
+                       }
+                   });
+                   try {
+                       Thread.sleep(50);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+               }
+            }
+        }).start();
     }
+
+
 }
